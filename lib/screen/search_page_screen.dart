cd0a6eb35382e348ed3cca24d05ff6e8a3fde8b1
Md. Foysal Joarder <foysaljoarder012@gmail.com>
2023-10-19 00:28:17 +0600

    now at this moment 85 % done because search page's search and another api calling and showing data done booking and profile section will be complete tomorrow insha allah

--- lib/screen/search_page_screen.dart
+++ lib/screen/search_page_screen.dart
@@ -1,5 +1,8 @@
+import 'package:covid_19_tacker/model%20class/country_state_model_class.dart';
 import 'package:covid_19_tacker/widget/custom_http_class.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:shimmer/shimmer.dart';
 
 class SearchPageScreen extends StatefulWidget {
   const SearchPageScreen({Key? key}) : super(key: key);
@@ -8,9 +11,16 @@ class SearchPageScreen extends StatefulWidget {
   State<SearchPageScreen> createState() => _SearchPageScreenState();
 }
 
-class _SearchPageScreenState extends State<SearchPageScreen> {
+class _SearchPageScreenState extends State<SearchPageScreen> with SingleTickerProviderStateMixin{
 
   TextEditingController searchController = TextEditingController();
+
+  late AnimationController animationController = AnimationController(
+      vsync: this,
+  duration: Duration(seconds: 3)
+  )..repeat();
+
+  CustomHttpClass customHttpClass = CustomHttpClass();
 
 
   @override
@@ -40,6 +50,17 @@ class _SearchPageScreenState extends State<SearchPageScreen> {
             TextFormField(
               controller: searchController,
 
+              onChanged: (value){
+                setState(() {
+
+                });
+              },
+
+              onEditingComplete: (){
+                setState(() {
+
+                });
+              },
               decoration: InputDecoration(
 
                 enabledBorder: OutlineInputBorder(
@@ -66,24 +87,89 @@ class _SearchPageScreenState extends State<SearchPageScreen> {
               height: MediaQuery.of(context).size.height * 0.01,
             ),
 
-            // Expanded(
-            //     child: FutureBuilder(
-            //       future: CustomHttpClass().fetchCountriesData(),
-            //         builder: (BuildContext context, snapshot){
-            //       return ListView.builder(
-            //           shrinkWrap: true,
-            //           reverse: false,
-            //           scrollDirection: Axis.vertical,
-            //           itemCount: snapshot.data!.length,
-            //           itemBuilder: (context,index){
-            //             return ListTile(
-            //
-            //               leading: Image(image: NetworkImage(snapshot.data![index]["countryInfo"]["flag"])),
-            //             );
-            //           }
-            //       );
-            //     })
-            // )
+            Expanded(
+                child: FutureBuilder<List<dynamic>>(
+                  future: customHttpClass.fetchCountriesData(),
+                    builder: (BuildContext context, snapshot) {
+                      if (!snapshot.hasData) {
+                        return Shimmer.fromColors(
+                            child: ListView.builder(
+                                shrinkWrap: true,
+                                reverse: false,
+                                scrollDirection: Axis.vertical,
+                                itemCount: 10,
+                                itemBuilder: (context, index) {
+                                  return ListTile(
+                                    leading: Container(
+                                          height: 50,
+                                      width: 50,
+                                      color: Colors.white,
+                                    ),
+
+                                    title: Container(
+                                      height: 10,
+                                      width: 89,
+                                      color: Colors.white,),
+                                    subtitle: Container(
+                                      height: 10,
+                                      width: 89,
+                                      color: Colors.white,),
+                                  );
+                                }
+                            ),
+                            baseColor: Colors.grey.shade700,
+                            highlightColor: Colors.grey.shade100,
+                        );
+                      } else {
+                        return ListView.builder(
+                            shrinkWrap: true,
+                            reverse: false,
+                            scrollDirection: Axis.vertical,
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (context, index) {
+                              var name = Text(snapshot.data![index]["country"]);
+
+                              if(searchController.text.isEmpty){
+                                return ListTile(
+                                  leading: Image(
+                                      height: 50,
+                                      width: 50,
+                                      image: NetworkImage(snapshot
+                                          .data![index]["countryInfo"]["flag"])),
+
+                                  title: Text(snapshot.data![index]["country"]),
+                                  subtitle: Text(snapshot.data![index]["cases"].toString()),
+                                );
+                              }else if(name.toString().toLowerCase().contains(searchController.text.toLowerCase())){
+                                return ListTile(
+                                  leading: Image(
+                                      height: 50,
+                                      width: 50,
+                                      image: NetworkImage(snapshot
+                                          .data![index]["countryInfo"]["flag"])),
+
+                                  title: Text(snapshot.data![index]["country"]),
+                                  subtitle: Text(snapshot.data![index]["cases"].toString()),
+                                );
+                              } else{
+                                return Container();
+                              }
+                              return ListTile(
+                                leading: Image(
+                                    height: 50,
+                                    width: 50,
+                                    image: NetworkImage(snapshot
+                                    .data![index]["countryInfo"]["flag"])),
+
+                                title: Text(snapshot.data![index]["country"]),
+                                subtitle: Text(snapshot.data![index]["cases"].toString()),
+                              );
+                            }
+                        );
+                      }
+                    }
+                    )
+            )
           ],
         ),
       ),
